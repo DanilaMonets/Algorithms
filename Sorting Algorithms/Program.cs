a23119c07f557090960b034c7ff857a21666f5f4
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2018-04-12 23:35:30 +0300

    Counting sort added.

--- Sorting Algorithms/Program.cs
+++ Sorting Algorithms/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Sorting_Algorithms
 {
-        public class Tree
+    public class Tree
     {
         public Tree left;
         public Tree right;
@@ -68,66 +68,102 @@ namespace Sorting_Algorithms
             Console.Write(node.key + " ");
         }
     }
-        static class Algorithm
+    static class Algorithm
+    {
+        public static void selectionSort(int[] arr, int n)
         {
-            public static void selectionSort(int[] arr, int n)
+            for (int i = 0; i < n; ++i)
             {
-                for (int i = 0; i < n; ++i)
+                int temp = arr[0];
+                for (int j = i + 1; j < n; ++j)
                 {
-                    int temp = arr[0];
-                    for (int j = i + 1; j < n; ++j)
+                    if (arr[i] > arr[j])
                     {
-                        if (arr[i] > arr[j])
-                        {
-                            temp = arr[i];
-                            arr[i] = arr[j];
-                            arr[j] = temp;
-                        }
-                    }
-                }
-            }
-
-            public static void insertionSort(int[] array)
-            {
-                int length = array.Length;
-
-                for (int i = 1; i < length; i++)
-                {
-                    int j = i;
-
-                    while ((j > 0) && (array[j] < array[j - 1]))
-                    {
-                        int k = j - 1;
-                        int temp = array[k];
-                        array[k] = array[j];
-                        array[j] = temp;
-
-                        j--;
-                    }
-                }
-            }
-            public static void bubbleSort(int[] a, int n)
-            {
-                for (int j = n - 1; j > 0; --j)
-                {
-                    for (int i = 0; i < j; ++i)
-                    {
-                        if (a[i] > a[i + 1])
-                        {
-                            int toSwap = a[i];
-                            a[i] = a[i + 1];
-                            a[i + 1] = toSwap;
-                        }
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
                     }
                 }
             }
         }
-        class MainClass
+
+        public static void insertionSort(int[] array)
         {
-            public static void Main(string[] args)
+            int length = array.Length;
+
+            for (int i = 1; i < length; i++)
             {
-                int[] arr = new int[] { -12, 5, 2, 12, 6, 1, 1, 0, 0, 7, 18 };
+                int j = i;
+
+                while ((j > 0) && (array[j] < array[j - 1]))
+                {
+                    int k = j - 1;
+                    int temp = array[k];
+                    array[k] = array[j];
+                    array[j] = temp;
+
+                    j--;
+                }
+            }
+        }
+        public static void bubbleSort(int[] a, int n)
+        {
+            for (int j = n - 1; j > 0; --j)
+            {
+                for (int i = 0; i < j; ++i)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        int toSwap = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = toSwap;
+                    }
+                }
+            }
+        }
+        public static void countingSort(int[] array)
+        {
+            int min = array.Min();
+            int fmin;
+            if (min < 0)
+            {
+                fmin = min / -1;
+            }
+            else if (min > 0)
+            {
+                fmin = -min;
+            }
+            else
+            {
+                fmin = 0;
+            }
+            int max = array.Max();
+
+            int[] arrIndex = new int[max - min + 1];
+
+            for (int i = 0; i < array.Length; ++i)
+            {
+                arrIndex[array[i] + fmin]++;
+            }
+
+            int j = 0;
+            for (int i = 0; i < arrIndex.Length; ++i)
+            {
+                while (arrIndex[i] > 0)
+                {
+                    array[j] = i - fmin;
+                    j++;
+                    arrIndex[i]--;
+                }
             }
         }
     }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { -12, 5, 2, 12, 6, 1, 1, 0, 0, 7, 18 };
+        }
+    }
+}
 
