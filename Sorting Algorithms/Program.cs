007d93583a9d099bae06fec94466ff4d6f3900bb
Danila Monets <danil.monets@gmail.com>
2018-04-12 23:29:32 +0300

    some changes

--- Sorting Algorithms/Program.cs
+++ Sorting Algorithms/Program.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace Sorting_Algorithms
 {
-    class Program
-    {
         static class Algorithm
         {
             public static void selectionSort(int[] arr, int n)
@@ -46,6 +44,21 @@ namespace Sorting_Algorithms
                     }
                 }
             }
+            public static void bubbleSort(int[] a, int n)
+            {
+                for (int j = n - 1; j > 0; --j)
+                {
+                    for (int i = 0; i < j; ++i)
+                    {
+                        if (a[i] > a[i + 1])
+                        {
+                            int toSwap = a[i];
+                            a[i] = a[i + 1];
+                            a[i + 1] = toSwap;
+                        }
+                    }
+                }
+            }
         }
         class MainClass
         {
@@ -55,4 +68,4 @@ namespace Sorting_Algorithms
             }
         }
     }
-}
+
